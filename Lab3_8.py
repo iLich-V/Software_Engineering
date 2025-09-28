a9b67692dd871d48123d09df9f6e17d83719c7ac
iLich-V <ilya13112005@gmail.com>
2025-09-28 21:45:58 +0500

    Create Lab3_8.py

---
+++ Lab3_8.py
@@ -0,0 +1,19 @@
+value = 0
+while value < 100:
+    if value == 0:
+        value += 10
+    elif value // 5 > 2:
+        value *= 5
+    else:
+        value -= 5
+    print(value)
+
+
+while value < 100:
+    if value == 0:
+        value += 10
+    elif value // 5 > 1:
+        value *= 5
+    else:
+        value -= 5
+    print(value)
